9b8df8d498c4a9af6524c3b7bb69db800581c000
PabloYuste <pabloyustedm@gmail.com>
2019-01-29 20:30:06 +0100

    NUMERICOS

--- src/basicospabloyustedelmazo/BasicosPabloYusteDelMazo.java
+++ src/basicospabloyustedelmazo/BasicosPabloYusteDelMazo.java
@@ -15,7 +15,29 @@ public class BasicosPabloYusteDelMazo {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        numericos();  // Llamada al método
+         
+    //Estos son los tipos básicos
+        byte elMasPeque;
+        short chiquitin;
+        int entero;
+        long duplicaEntero;
+        float grande;
+        double doble;
+
+        //Ejemplos de asignación de valores a las variables de tipos básicos
+        entero = 10;
+        doble = 5.6;
+
+        //Mostrar en pantalla el contendido de las variables de tipos básicos.
+        System.err.println("Valor entero");
+        System.out.println(entero);
+        System.out.println("Valor doble");
+        System.out.println(doble);
+    }
+
+    private static void numericos() {
+    
     }
     
 }
