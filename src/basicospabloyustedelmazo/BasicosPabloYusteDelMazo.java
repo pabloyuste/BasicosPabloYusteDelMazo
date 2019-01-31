ceb976994b92912b1d8814229532e62437cf5085
PabloYuste <pabloyustedm@gmail.com>
2019-01-31 20:38:01 +0100

    MAL....

--- src/basicospabloyustedelmazo/BasicosPabloYusteDelMazo.java
+++ src/basicospabloyustedelmazo/BasicosPabloYusteDelMazo.java
@@ -5,6 +5,8 @@
  */
 package basicospabloyustedelmazo;
 
+import java.util.Scanner;
+
 /**
  *
  * @author usuario
@@ -14,8 +16,27 @@ public class BasicosPabloYusteDelMazo {
     /**
      * @param args the command line arguments
      */
-    public static void main(String[] args) {
-        // TODO code application logic here
+public static void main(String[] args) {
+    String  respuesta;
+        Scanner reader = new Scanner(System.in);
+        System.out.println("Escribe 'x' o 'y':");
+        System.out.println(reader.next());
+        respuesta = reader.next();
+
+        //Estos son los tipos básicos
+        boolean x;
+        boolean y;
+
+        //Ejemplos de asignación de valores a las variables de tipos básicos
+            if (respuesta = x);
+
+        //Mostrar en pantalla el contendido de las variables de tipos básicos.
+
+        
+        
+        
+       
+    
     }
     
 }
