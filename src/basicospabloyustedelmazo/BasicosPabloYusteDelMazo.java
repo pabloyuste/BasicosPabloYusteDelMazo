3f4f9742b1da1531f301cadfffa3bea047d4900f
PabloYuste <pabloyustedm@gmail.com>
2019-01-31 19:58:02 +0100

    CADENAS

--- src/basicospabloyustedelmazo/BasicosPabloYusteDelMazo.java
+++ src/basicospabloyustedelmazo/BasicosPabloYusteDelMazo.java
@@ -14,8 +14,22 @@ public class BasicosPabloYusteDelMazo {
     /**
      * @param args the command line arguments
      */
-    public static void main(String[] args) {
-        // TODO code application logic here
+public static void main(String[] args) {
+        cadenas();  // Llamada al método
+        
+    }
+
+    private static void cadenas() {
+    //Estos son los tipos básicos
+        String letras;
+
+        //Ejemplos de asignación de valores a las variables de tipos básicos
+        letras = "hola mundo";
+
+        //Mostrar en pantalla el contendido de las variables de tipos básicos.
+        System.err.println("Variable letras");
+        System.out.println(letras);
+    
     }
     
 }
